17f6ec5eaa902ac358c8b530491ed0f3b7e16965
Jokur7 <63812881+Jokur7@users.noreply.github.com>
2021-04-21 22:08:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -310,3 +310,101 @@ function checkTie() {
 	return false;
 }
 }
+/* nowe testy */
+function scroll_to(){
+    $('html, body').animate({
+        scrollTop: $('#screen').offset().top
+    },1000);
+}
+function show_image(src) {
+    var img = document.createElement("img");
+    img.src = src;
+    document.getElementById('screen').appendChild(img);
+}
+
+
+
+function loadInt(){
+    /*var divGameBoard= $('.game-board');*/
+    
+    /*var divPlayer1=$('.player1');
+    var divPlayer2=$('.player2');
+    var divPodzial=$('#podzial');
+    var divPkt= $('.pkt');
+    var divStartGame=$('.title4');
+
+    divGameBoard.empty();
+    divPlayer1.empty();
+    divPlayer2.empty();
+    divPodzial.empty();
+    divPkt.empty();
+    divStartGame.empty();
+    divStartGame.css('background-color','#282828');
+
+    divStartGame.off("mouseover mouseout");*/
+    var divScreen=$('#pomoc');
+    divScreen.text('Interstellar is a 2014 epic science fiction film directed, co-written and co-produced by Christopher Nolan. It stars Matthew McConaughey, Anne Hathaway, Jessica Chastain, Bill Irwin, Ellen Burstyn, Matt Damon, and Michael Caine. Set in a dystopian future where humanity is struggling to survive, the film follows a group of astronauts who travel through a wormhole near Saturn in search of a new home for humanity.Interstellar premiered on October 26, 2014, in Los Angeles, California. The film had a worldwide gross of over $677 million, making it the tenth-highest-grossing film of 2014. Interstellar received positive reviews for its screenplay, direction, themes, visual effects, musical score, acting, and ambition. At the 87th Academy Awards, the film won the Academy Award for Best Visual Effects, and was nominated for Best Original Score, Best Sound Mixing, Best Sound Editing and Best Production Design.');
+
+   scroll_to();
+}
+
+
+
+function loadOnce(){
+    var divGameBoard= $('.game-board');
+    
+    var divPlayer1=$('.player1');
+    var divPlayer2=$('.player2');
+    var divPodzial=$('#podzial');
+    var divPkt= $('.pkt');
+    var divStartGame=$('.title4');
+
+    divGameBoard.empty();
+    divPlayer1.empty();
+    divPlayer2.empty();
+    divPodzial.empty();
+    divPkt.empty();
+    divStartGame.empty();
+    divStartGame.css('background-color','#282828');
+
+    divStartGame.off("mouseover mouseout");
+
+    var divScreen=$('#pomoc');
+    divScreen.text('Once Upon a Time in Hollywood is a comedy-drama film written and directed by Quentin Tarantino. It features a cast led by Leonardo DiCaprio, Brad Pitt, and Margot Robbie. Set in 1969 Los Angeles, the film follows an actor and his stunt double as they navigate the changing film industry, and features "multiple storylines in a modern fairy tale tribute to the final moments of Hollywoods golden age. Once Upon a Time in Hollywood premiered on May 21, 2019. The film has grossed $374 million worldwide and received praise from critics for Tarantinos screenplay and direction, acting, cinematography, costume design, production values, and soundtrack. The film was chosen by the American Film Institute and the National Board of Review as one of the top ten films of the year. It received 10 nominations at the 92nd Academy Awards, including Best Picture, and won Best Supporting Actor (Pitt) and Best Production Design. It also won Best Motion Picture – Musical or Comedy at the 77th Golden Globe Awards.');
+
+    scroll_to();
+}
+
+function loadMatrix(){
+    var divGameBoard= $('.game-board');
+    
+    var divPlayer1=$('.player1');
+    var divPlayer2=$('.player2');
+    var divPodzial=$('#podzial');
+    var divPkt= $('.pkt');
+    var divStartGame=$('.title4');
+
+    divGameBoard.empty();
+    divPlayer1.empty();
+    divPlayer2.empty();
+    divPodzial.empty();
+    divPkt.empty();
+    divStartGame.empty();
+    divStartGame.css('background-color','#282828');
+
+    divStartGame.off("mouseover mouseout");
+
+    var divScreen=$('#pomoc');
+    divScreen.text('The Matrix is a 1999 science fiction action film written and directed by the Wachowskis. It stars Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving, and Joe Pantoliano. It depicts a dystopian future in which humanity is unknowingly trapped inside a simulated reality, the Matrix, created by intelligent machines to distract humans while using their bodies as an energy source. When computer programmer Thomas Anderson, under the hacker alias "Neo", uncovers the truth, he "is drawn into a rebellion against the machines" along with other people who have been freed from the Matrix. The Matrix was on March 31, 1999, and grossed over $460 million worldwide. It won four Academy Awards, as well as other accolades, including BAFTA Awards and Saturn Awards. The Matrix was praised for its innovative visual effects, action sequences, cinematography and entertainment value. The film is considered to be among the best science fiction films of all time.');
+
+    scroll_to();
+}
+
+function loadAvatar()
+{
+
+    var divScreen=$('#pomoc');
+    divScreen.text('Avatar is a 2009 American epic science fiction film directed, written, produced, and co-edited by James Cameron and stars Sam Worthington, Zoe Saldana, Stephen Lang, Michelle Rodriguez, and Sigourney Weaver. The film is set in the mid-22nd century when humans are colonizing Pandora, a lush habitable moon of a gas giant in the Alpha Centauri star system, in order to mine the mineral unobtanium, a room-temperature superconductor. The expansion of the mining colony threatens the continued existence of a local tribe of Navi – a humanoid species indigenous to Pandora. Avatar premiered in London on December 10, 2009, with critics highly praising its groundbreaking visual effects. During its theatrical run, the film broke several box office records and became the highest-grossing film at the time, surpassing Camerons Titanic, which had held those records for twelve years. Avatar remained the highest-grossing film worldwide for nearly a decade, before being overtaken by Avengers: Endgame in 2019. To this day, Avatar remains the second highest-grossing movie of all time when adjusted for inflation after Gone with the Wind with a total of more than $3 billion. It also became the first film to gross more than $2 billion. Avatar was nominated for nine Academy Awards, including Best Picture and Best Director, and won three, for Best Art Direction, Best Cinematography and Best Visual Effects.');
+
+   scroll_to();
+}
